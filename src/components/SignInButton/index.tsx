3414b3458ef2005600f48c738435bdbf91af96c5
vinicius martins <vinicius.cross07@gmail.com>
2021-12-08 11:38:16 -0300

    feat:github-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,30 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signOut, signIn, useSession } from "next-auth/react";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { status, data } = useSession();
+
+  const isUserLoggedIn = status === "authenticated" ? true : false;
+  console.log(isUserLoggedIn, status);
+
   return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+    <button
+      onClick={() => signOut()}
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color="#04d361" />
-      Vinicius Martins
+      {data.user?.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      onClick={() => signIn("github")}
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
