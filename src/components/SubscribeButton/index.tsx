ebaed236d5cdff198a6ca09a4de60338c7ebed26
vinicius martins <vinicius.cross07@gmail.com>
2021-12-08 17:29:48 -0300

    stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { useSession, signIn } from "next-auth/react";
+import { api } from "../../services/api";
 import styles from "./styles.module.scss";
 
 interface SubscribButtonProps {
@@ -5,6 +7,21 @@ interface SubscribButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribButtonProps) {
+  const { status } = useSession();
+
+  async function handleSubscribe() {
+    if (status !== "authenticated") {
+      signIn("github");
+      return;
+    }
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
